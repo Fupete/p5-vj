86b6fa0950d2a4ceff3d86faf19c3d1a58942c12
d-t <hereisfupete@gmail.com>
2022-05-03 09:47:40 +0200

    test/sketches minors

--- art/0/0/0.js
+++ art/0/0/0.js
@@ -1,17 +1,15 @@
-var s1 = function (sketch) {
-    sketch.setup = function () {
-        let cnv = sketch.createCanvas(sketch.displayWidth, sketch.displayHeight);
-        cnv.parent("canvas");
-        sketch.background(0);
-
+var s1 = function(s) {
+  s.setup = function() {
+    let cnv = s.createCanvas(s.windowWidth, s.windowHeight);
+    cnv.parent("canvas");
+    s.background(255);
+  }
+  s.draw = function() {
+    s.fill(0);
+    s.stroke(255);
+    r = 50;
+    for (var i = 0; i < 100; i++) {
+      s.ellipse(s.random(s.width), s.random(s.height), r, r);
     }
-    sketch.draw = function () {
-        sketch.fill(255);
-        r = 10;
-        for (var i = 0; i < 100; i++) {
-            sketch.ellipse(sketch.random(sketch.width), sketch.random(sketch.height), r, r);
-        }
-    }
+  }
 }
-
-
--- art/0/0/1.js
+++ art/0/0/1.js
@@ -1,14 +1,14 @@
-var s1 = function (sketch) {
-    sketch.setup = function () {
-        let cnv = sketch.createCanvas(sketch.displayWidth, sketch.displayHeight);
-        cnv.parent("canvas");
-        sketch.background(0);
+var s1 = function(s) {
+  s.setup = function() {
+    let cnv = s.createCanvas(s.windowWidth, s.windowHeight);
+    cnv.parent("canvas");
+    s.background(0);
+  }
+  s.draw = function() {
+    s.fill(255);
+    r = 10;
+    for (var i = 0; i < 100; i++) {
+      s.rect(s.random(s.width), s.random(s.height), r, r);
     }
-    sketch.draw = function () {
-        sketch.fill(255);
-        r = 10;
-        for (var i = 0; i < 100; i++) {
-            sketch.rect(sketch.random(sketch.displayWidth), sketch.random(sketch.displayHeight), r, r);
-        }
-    }
+  }
 }
